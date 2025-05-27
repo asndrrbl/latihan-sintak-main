308912d24f4a8a4daf1b68f73886552878eda843
asndrrbl <auliasandrasalsabila1176@gmail.com>
2025-05-27 14:23:26 +0700

    menambahkan id card

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="footer sm:footer-horizontal items-center p-4 border border-gray-300">
       <aside className="grid-flow-col items-center">
         <CodeXml />
-        <p>Copyright © Kgs. M. Rafiussyakban {new Date().getFullYear()} - All right reserved</p>
+        <p>Copyright © Aulia Sandra Salsabila {new Date().getFullYear()} - All right reserved</p>
       </aside>
       <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
         <a href='http://facebook.com'>
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import NavBar from "../components/navbar";
 import Footer from "../components/footer";
-import RafiNameCard from "../components/name-card/rafi";
-import SandraNameCard from "../components/name-card/rafi";
+import SandraNameCard from "../components/name-card/sandra";
 
 function AboutPage() {
   return (
@@ -11,8 +10,7 @@ function AboutPage() {
       <div className="min-h-screen py-24 ">
         <div className="flex gap-8 flex-wrap items-center justify-center w-full px-4">
         
-        <SandraNameCardNameCard />
-        {/* Panggil kartu nama kamu dibawah sini..... */}
+        <SandraNameCard/>
 
 
         </div>
